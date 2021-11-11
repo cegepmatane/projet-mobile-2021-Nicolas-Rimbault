96eb6b7fb176b8151342f670fd088b45fde3171a
TMRomain <romainover51@gmail.com>
2021-11-11 14:15:12 -0500

    Ajout du plugin de dialog

--- QRArt/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ QRArt/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -31,10 +31,27 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "CameraPopoverHandle"
       ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification",
+      "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification_android",
+      "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.5",
-    "cordova-plugin-camera": "5.0.3"
+    "cordova-plugin-camera": "5.0.3",
+    "cordova-plugin-dialogs": "2.0.2"
   };
 });

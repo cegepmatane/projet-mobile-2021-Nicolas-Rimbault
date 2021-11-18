a1ec7523f874f3b3ee500af80e3683fa7bca0e60
TMRomain <romainover51@gmail.com>
2021-11-18 14:56:58 -0500

    Changement dans les lib

--- QRArt/platforms/android/app/src/main/assets/www/vue/VuePrincipale.js
+++ QRArt/platforms/android/app/src/main/assets/www/vue/VuePrincipale.js
@@ -5,6 +5,7 @@ class VuePrincipale{
     
     this.htmlMenu = document.getElementById("html-vue-menu").innerHTML; //implémenter le menu dans la vue
     this.html = this.html.replace("{menu}", this.htmlMenu);
+    
     //Setup du lecteur de QRcode
    // QrScanner.WORKER_PATH = '../lib/qr-scanner-worker.min.js';
   }
@@ -12,35 +13,13 @@ class VuePrincipale{
 
   afficher(){
     document.getElementsByTagName("body")[0].innerHTML = this.html;
-    
-    function onDeviceReady() {
-      console.log('Device Ready');
-      alert("Device Ready");
+        alert("test1");
 
-        // Now safe to use device APIs
-        var done = function(err, status){
-          if(err){
-            console.error(err._message);
-            console.log('QRScanner Echec');
-            alert("test1");
-  
-  
-          } else {
-            console.log('QRScanner is initialized. Status:');
-            console.log(status);
-            alert("test2");
+    QrScanner.WORKER_PATH = '../lib/qr-scanner-worker.min.js';
 
-  
-          }
-        };
-  
-        window.QRScanner.prepare(done);
-    }
-    document.addEventListener("deviceready", onDeviceReady, false);
-
+    alert("test");
     // Grab elements, create settings, etc.
     var video = document.getElementById('video');
-    console.log("TEST2");
 
     if(mobileCheck()){
       var constraints = navigator.mediaDevices.getSupportedConstraints();
@@ -56,8 +35,6 @@ class VuePrincipale{
           video.play();
       });
     }else{
-          console.log("TEST8");
-
       // Get access to the camera!
       if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
         // Not adding `{ audio: true }` since we only want video now

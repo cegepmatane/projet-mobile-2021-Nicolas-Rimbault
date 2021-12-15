ac1fd3dd7b763709eff6e5239dc075db86ad8b04
TMRomain <romainover51@gmail.com>
2021-12-15 18:28:12 -0500

    Ajout lib + logique swipe

--- QRArt/platforms/android/app/src/main/assets/www/Application.js
+++ QRArt/platforms/android/app/src/main/assets/www/Application.js
@@ -1,4 +1,5 @@
-﻿class Application {
+﻿
+class Application {
   constructor(window, vuePrincipale, itemDAO, vueListeItem, vueParametre, vueItem, vueConnexion, vueInscription, vueCreation, vueReussite) {
 
     this.window = window;
@@ -11,6 +12,7 @@
     this.vueInscription = vueInscription;
     this.vueCreation = vueCreation;
     this.vueReussite = vueReussite;
+    let currentScreen = 1;
 
     console.log("Chargement");
 
@@ -18,14 +20,48 @@
     this.window.addEventListener("hashchange", () => this.naviguer());
 
     this.naviguer();
+
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+      var touchArea = document.getElementsByTagName("body")[0];
+      var myRegion = new ZingTouch.Region(touchArea,undefined,false)
+      myRegion.bind(touchArea, 'swipe', function(e){
+        if(90 < e.detail.data[0].currentDirection && e.detail.data[0].currentDirection < 270){
+          console.log("Droite SWIPE");
+          allerDroite();
+        } else{
+          console.log("Gauche SWIPE");
+          allerGauche();
+        }
+      });
+    }
+    function allerDroite(){
+      if(currentScreen == 1){
+        window.location.hash = '#parametre';
+        currentScreen =2;
+      }else if (currentScreen == 0){
+        window.location.hash = '#principale';
+        currentScreen =1;
+      }
+    }
+    function allerGauche(){
+      if(currentScreen == 1){
+        window.location.hash = '#liste-item';
+        currentScreen =0;
+      }else if (currentScreen == 2){
+        window.location.hash = '#principale';
+        currentScreen =1;
+      }
+    }
   }
 
   naviguer() {
     let hash = window.location.hash;
-    if (!hash) {
+    if (!hash || hash.match(/^#principale/)) {
       document.body.style.backgroundColor= "transparent";
       this.vuePrincipale.afficher();
     } else if (hash.match(/^#liste-item/)) {
+      document.body.style.backgroundColor = "black";
       this.vueListeItem.initialiserListeItem(this.itemDAO.lister());
       this.vueListeItem.afficher();
     } else if (hash.match(/^#parametre/)) {
@@ -40,7 +76,7 @@
     }else if (hash.match(/^#creation/)) {
       document.body.style.backgroundColor = "black";
       this.vueCreation.afficher();
-    } else {
+    }else{
       let navigation = hash.match(/^#item\/([0-9]+)/);
       let idItem = navigation[1];
 
